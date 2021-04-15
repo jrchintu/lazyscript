ec915fe351fb0f66214365fb4fb8f3340124ca0e
JR CHINTU <75427894+jrchintu@users.noreply.github.com>
2021-04-15 12:03:42 +0530

    Delete add_hals_mido-a11.sh

--- add_hals_mido-a11.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-alias gcl='git clone'
-cd hardware/qcom-caf/msm8996 
-rm -rf audio media display 
-gcl https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.1-caf-msm8996 media 
-gcl https://github.com/LineageOS/android_hardware_qcom_display -b lineage-18.1-caf-msm8996 display 
-gcl https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.1-caf-msm8996 audio 
