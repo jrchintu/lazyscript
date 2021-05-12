35d37d6b9e0afa3682af21d4d5c53abbbde63310
JR CHINTU <75427894+jrchintu@users.noreply.github.com>
2021-05-12 13:55:58 +0000

    Update bash_alias.sh

--- bash_alias.sh
+++ bash_alias.sh
@@ -2,6 +2,7 @@
 
 git config --global user.name "jrchintu"
 git config --global user.email "harrypetersun@outlook.com.com"
+git config --global push.default matching
 export TZ='Asia/Kolkata'
 
 #alias
