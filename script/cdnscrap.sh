5fb28cd1b48cb89750c2310966f7b2bc51aae05f
CHINTU <harrypetersun@outlook.com>
2021-06-27 19:42:15 +0530

    Phix wrong formatting

--- script/cdnscrap.sh
+++ script/cdnscrap.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-printf "https://s3-ap-southeast-1.amazonaws.com/paper-%s.pdf\n" {1111..9999}>./url.txt
+printf "https://s3-ap-southeast-1.amazonaws.com/paper-%s.pdf\n" {1111..9999} >./url.txt
 
 cat ./url.txt | while read LINE; do
     echo "$LINE"
     export FILETYPE="$(curl -sI "$LINE" | grep Content-Type | cut -d: -f2 | cut -d/ -f2)"
     if [[ "$FILETYPE" = "pdf" ]]; then
-        echo "$LINE" >> ok.txt
+        echo "$LINE" >>ok.txt
         echo "Working"
     else
         echo "FucK"
